0338c29f110e40701b0eb7a5d1a6f1c1f1590d42
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-12 13:24:48 +0000

    最初のMutex

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
-use std::sync::mpsc;
-use std::time::Duration;
-use std::{thread, vec};
+use std::sync::Mutex;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let tx1 = mpsc::Sender::clone(&tx);
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
     }
+    println!("m={:?}", m);
 }

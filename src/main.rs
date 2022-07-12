301ca5dcde3805a71b6c7a1aa904b146c52cbf5c
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-12 12:27:46 +0000

    転送機をクローンして複数の生成器を作成する。

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::sync::mpsc;
-use std::thread;
 use std::time::Duration;
+use std::{thread, vec};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
 
     thread::spawn(move || {
         let vals = vec![
@@ -12,9 +13,24 @@ fn main() {
             String::from("the"),
             String::from("thread"),
         ];
+
         for val in vals {
             tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(2));
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
         }
     });
 

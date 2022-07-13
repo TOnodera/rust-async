7bb63ca2fe7cc84258a29538ed0b6204d6913666
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-13 11:43:58 +0000

    独自参照外しの使用

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,11 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+fn hello(s: &str) {
+    println!("Hello {}", s);
+}
+
 fn main() {
-    let x = 5;
-    let y = MyBox::new(x);
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }

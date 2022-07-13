d3ce2f8dfef54db2456de698381efb10ce1596da
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-13 13:01:29 +0000

    シングルスレッドサーバー

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
-
-use std::rc::Rc;
-use List::{Cons, Nil};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "document/index.html")
+    } else {
+        ("HTTP/1.1 400 OK\r\n\r\n", "document/404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }

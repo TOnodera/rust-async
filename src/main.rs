620b523083febf48ba05b43fe2f66a7d84f4af48
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-13 11:49:20 +0000

    Dropの実験

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data: {}", self.data);
     }
-}
-
-fn hello(s: &str) {
-    println!("Hello {}", s);
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointer created.");
 }

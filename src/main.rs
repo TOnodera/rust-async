0e424d3e96e36f8003ebb52d5b67ef60ad713222
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-13 12:01:27 +0000

    Reference counterで参照をつくる。

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data: {}", self.data);
-    }
-}
+use std::rc::Rc;
+use List::{Cons, Nil};
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-    println!("CustomSmartPointer created.");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }

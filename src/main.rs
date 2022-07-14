e161c829bbd3894f8d1221a28e920ac7f886ccaa
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-07-14 12:50:33 +0000

    /sleep

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs::File,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -18,8 +20,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "document/index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "document/index.html")
     } else {
         ("HTTP/1.1 400 OK\r\n\r\n", "document/404.html")
